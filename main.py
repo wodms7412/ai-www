43bd37db1ed650db761addef5e3dcd0026e3efc4
wodms7412 <motive56330@gmail.com>
2025-11-04 13:54:01 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
 st.title('나의 첫 웹 서비스 만들기!')
-st.text_input('이름 말해라 준호야')
-st.button('인사준호 생성')
+name=st.text_input('이름 말해라 준호야')
+if st.button('인사준호 생성'):
+  st.write(name+'님! 안녕준호!')
