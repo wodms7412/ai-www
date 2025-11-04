7745eb1aa451ee251d6c2588e4c5c6b05c6843ff
wodms7412 <motive56330@gmail.com>
2025-11-04 14:04:49 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,6 @@ st.title('나의 첫 웹 서비스 만들기!')
 name=st.text_input('이름 말해라 준호야')
 st.selectbox('좋아하는 음식준호 골라주세염',['치킨','펩시콜라','준호',])
 if st.button('인사준호 생성'):
-  st.write(name+'님! 안녕준호!')
+  st.info(name+'님! 안녕준호!')
+  st.warning(menu+'을(를)종아하나 준호!,나도 좋아우주')
+  st.error('안녕안녕하세요!!')
